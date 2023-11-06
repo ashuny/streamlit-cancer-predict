debc82ff0060b30ba3276a17248ea078970314c8
Shun Yu <ashuny2010@gmail.com>
2023-11-05 17:48:36 -0800

    add radar chart to the page

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pickle5 as pickle
 import pandas as pd
+import plotly.graph_objects as go
 
 
 def get_clean_data():
@@ -60,6 +61,74 @@ def add_sidebar():
 
     return input_dict
 
+def get_radar_chart(input_data):
+  
+  input_data = get_scaled_values(input_data)
+  
+  categories = ['Radius', 'Texture', 'Perimeter', 'Area', 
+                'Smoothness', 'Compactness', 
+                'Concavity', 'Concave Points',
+                'Symmetry', 'Fractal Dimension']
+
+  fig = go.Figure()
+
+  fig.add_trace(go.Scatterpolar(
+        r=[
+          input_data['radius_mean'], input_data['texture_mean'], input_data['perimeter_mean'],
+          input_data['area_mean'], input_data['smoothness_mean'], input_data['compactness_mean'],
+          input_data['concavity_mean'], input_data['concave points_mean'], input_data['symmetry_mean'],
+          input_data['fractal_dimension_mean']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Mean Value'
+        ))
+  fig.add_trace(go.Scatterpolar(
+        r=[
+          input_data['radius_se'], input_data['texture_se'], input_data['perimeter_se'], input_data['area_se'],
+          input_data['smoothness_se'], input_data['compactness_se'], input_data['concavity_se'],
+          input_data['concave points_se'], input_data['symmetry_se'],input_data['fractal_dimension_se']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Standard Error'
+  ))
+  fig.add_trace(go.Scatterpolar(
+        r=[
+          input_data['radius_worst'], input_data['texture_worst'], input_data['perimeter_worst'],
+          input_data['area_worst'], input_data['smoothness_worst'], input_data['compactness_worst'],
+          input_data['concavity_worst'], input_data['concave points_worst'], input_data['symmetry_worst'],
+          input_data['fractal_dimension_worst']
+        ],
+        theta=categories,
+        fill='toself',
+        name='Worst Value'
+  ))
+
+  fig.update_layout( 
+    polar=dict(
+        radialaxis=dict(
+        visible=True,
+        range=[0, 1] 
+        )),
+    showlegend=True
+    )
+  
+  return fig
+
+def get_scaled_values(input_dict):
+    data = get_clean_data()
+    x = data.drop(['diagnosis'], axis=1)
+
+    scaled_dict={}
+
+    for key, value in input_dict.items():
+        max_val = x[key].max()
+        min_val = x[key].min()
+        scaled_value = (value - min_val) / (max_val - min_val)
+        scaled_dict[key] = scaled_value
+    
+    return(scaled_dict)
 
 def main():
     st.set_page_config(
@@ -74,8 +143,6 @@ def main():
 
     # st.write(input_data)
 
-    
-
     with st.container():
         st.title("Breast Cancer Predictor")
         st.write("Please connect this app to your cytology lab to help diagnose breast cancer form your tissue sample. This app predicts using a machine learning model whether a breast mass is benign or malignant based on the measurements it receives from your cytosis lab. You can also update the measurements by hand using the sliders in the sidebar. ")
@@ -84,6 +151,8 @@ def main():
 
     with col1:
         st.write("this is column 1")
+        radar_chart = get_radar_chart(input_data)
+        st.plotly_chart(radar_chart)
     with col2:
         st.write("this is column 2")
 
